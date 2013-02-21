54fc45e622372c53b92fd6ef5e85bb080629b237
Ted Lorts <tplorts@gmail.com>
2013-02-21 17:51:50 -0500

    adding in things to new file now.

--- QueryMMWR.php
+++ QueryMMWR.php
@@ -8,12 +8,27 @@ libxml_use_internal_errors( true );
 $isTest = ( array_key_exists("test", $_GET) );
 if( $isTest )
 	echo "<p>This is a test.</p>";
-else {
 
-	if( $isTest && !array_key_exists("diseaseName", $_GET) )
-		$queriedDisease = "Chlamydia trachomatis infection";
-	else
-		$queriedDisease = $_GET["diseaseName"];
-		
-	echo $queriedDisease;
-}
+if( $isTest && !array_key_exists("diseaseName", $_GET) )
+	$queriedDisease = "Chlamydia trachomatis infection";
+else
+	$queriedDisease = $_GET["diseaseName"];
+
+if( $isTest )
+	echo "<p>Disease: '".$queriedDisease."'</p>";
+
+
+$stateArray = getOldGPOStateAbbreviations();
+if( $isTest )
+	echo var_dump( $stateArray );
+
+$mmwrTimeOptions = new DOMDocument();		
+$mmwrTimeOptions->loadHTMLFile("http://wonder.cdc.gov/mmwr/mmwrmorb.asp");
+$qpMmwrTimeOptions = qp($mmwrTimeOptions->saveHTML());
+
+$yearPresent = $qpMmwrTimeOptions->branch()->find("select[name='mmwr_year'] option[selected]")->text();
+$weekPresent = $qpMmwrTimeOptions->find("select[name='mmwr_week'] option[selected]")->text();
+
+if( $isTest )
+	echo "Using year ".$yearPresent." and week number ".$weekPresent.".<br />";
+
--- mmwr-tables.php
+++ mmwr-tables.php
@@ -1,4 +1,3 @@
-
 <?php
 
 require 'QueryPath/QueryPath.php';
