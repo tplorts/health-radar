cb87f2c3b04667875c8e46306a0bb06c9ae0ccfc
Ted Lorts <tplorts@gmail.com>
2013-02-22 09:29:26 -0500

    min versions of js libs and adding stuff back into querymmwr

--- php/index.php
+++ php/index.php
@@ -4,11 +4,11 @@
     <title>Health Radar -- Chlamydia cases reported within the past week</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-		<script src="http://code.jquery.com/jquery-latest.js"></script>
-		<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
-		<script type="text/javascript" src="jquery.csv-0.71.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js" ></script>
+		<script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js" ></script>
+		<script type="text/javascript" src="jquery.csv-0.71.min.js" ></script>
 		
-		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css">
 		
 
 	<style>
