d0f7bdc45b713ea78994a27aea34b786a6d7cae6
Ted Lorts <tplorts@gmail.com>
2013-02-23 14:48:56 -0500

    added stages of test file so that with parameter we can progess piece by piece:

--- testQueryMMWR.php
+++ testQueryMMWR.php
@@ -6,7 +6,17 @@ require 'MMWRTableInfo.php';
 
 libxml_use_internal_errors( true );
 
-echo "<p>This is a test.</p>";
+
+
+if( !array_key_exists("level", $_GET) )
+	$testlevel = intval( $_GET["level"] );
+else
+	$testlevel = 0;
+
+echo "<p>This is a test, level ".$testlevel.".</p>";
+
+$stage = 0;
+
 
 if( !array_key_exists("diseaseName", $_GET) )
 	$queriedDisease = "Chlamydia trachomatis infection";
@@ -15,39 +25,49 @@ else
 
 echo "<p>Disease: '".$queriedDisease."'</p>";
 
+if( $testlevel == ++$stage ) exit();
 
 $stateArray = getOldGPOStateAbbreviations();
 echo "<p>".var_dump( $stateArray )."</p>";
 
+if( $testlevel == ++$stage ) exit();
 
 $mmwrTimeOptions = new DOMDocument();		
 $mmwrTimeOptions->loadHTMLFile("http://wonder.cdc.gov/mmwr/mmwrmorb.asp");
 $qpMmwrTimeOptions = qp($mmwrTimeOptions->saveHTML());
 
+if( $testlevel == ++$stage ) exit();
+
 $yearPresent = $qpMmwrTimeOptions->branch()->find("select[name='mmwr_year'] option[selected]")->text();
 $weekPresent = $qpMmwrTimeOptions->find("select[name='mmwr_week'] option[selected]")->text();
 
 echo "<p>Using year ".$yearPresent." and week number ".$weekPresent.".</p>";
-	
+
+if( $testlevel == ++$stage ) exit();
+
 echo "<p>".var_dump( $diseaseTableIds )."</p>";
+
+if( $testlevel == ++$stage ) exit();
 
 $d = $diseaseTableIds[ $queriedDisease ];
 $t = $d[0];
 $c = 2 + 5*($d[1] - 1);
-
 echo "<p>For disease '".$queriedDisease."' we will query table ".$t." and look to column number ".$c.".</p>";
-
 $mmwrTableUrl = "http://wonder.cdc.gov/mmwr/mmwr_reps.asp?mmwr_year=".$yearPresent."&mmwr_week=".$weekPresent."&mmwr_table=".$t;
-
 echo "<p>About to request <a href='".$mmwrTableUrl."'>".$mmwrTableUrl."</a>.</p>";
 
+if( $testlevel == ++$stage ) exit();
 
 $doc = new DOMDocument();		
 $doc->loadHTMLFile($mmwrTableUrl);
 $qpMMWR = qp($doc->saveHTML());
 $qpMMWRTable = $qpMMWR->find("table:eq(8)");
 
+if( $testlevel == ++$stage ) exit();
+
 echo $qpMMWRTable->branch()->html();
+
+if( $testlevel == ++$stage ) exit();
 
 $illnessCases = array();
 foreach( $qpMMWRTable->find("tr") as $caseRow ) {
@@ -62,5 +82,8 @@ foreach( $qpMMWRTable->find("tr") as $caseRow ) {
 		$illnessCases[ $stateArray[$stateAbbr] ] = $casesThisWeek;
 	}
 }
+
+if( $testlevel == ++$stage ) exit();
+
 echo json_encode( $illnessCases );
 
